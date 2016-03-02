ea9969d7d71bd1ed139c6a55cc323dc92f3c09ac
Simon Burns <simon_burns@uk.ibm.com>
2016-03-02 20:19:33 +0000

    feat(common-ui): Clean up
    Add copyright to JS
    Rename files for consistency
    Extract out extends and mixins
    
    Work item #4
    
    DC 1.1 Signed-off-by: Simon Burns <simon_burns@uk.ibm.com>

--- app/app.js
+++ app/app.js
@@ -1,41 +1,56 @@
+/**
+ * Copyright 2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 'use strict';
 (function () {
 
   angular
-  .module('watson.common.ui', [
-    "ngAnimate",
-    "ui.bootstrap",
-    "ui.router",
-    "watson.common.ui.controllers"
-  ])
-  .config(appConfig);
-
+    .module('watson.common.ui', [
+      "ngAnimate",
+      "ui.bootstrap",
+      "ui.router",
+      "watson.common.ui.controllers"
+    ])
+    .config(appConfig);
 
   // App config
-  function appConfig($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+  function appConfig($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider
-    .otherwise('/overview');
+      .otherwise('/overview');
 
     $stateProvider
-    .state('overview', {
-      url: '/overview',
-      templateUrl: 'guide/overview.html'
-    })
-    .state('elements', {
-      url: '/elements',
-      templateUrl: 'guide/elements.html'
-    })
-    .state('components', {
-      url: '/components',
-      templateUrl: 'guide/components.html',
-      controller: 'GuideController',
-      controllerAs: 'ctrl'
-    })
-    .state('layout', {
-      url: '/layout',
-      templateUrl: 'guide/layout.html'
-    });
-  }
+      .state('overview', {
+        url: '/overview',
+        templateUrl: 'guide/overview.html'
+      })
+      .state('elements', {
+        url: '/elements',
+        templateUrl: 'guide/elements.html'
+      })
+      .state('components', {
+        url: '/components',
+        templateUrl: 'guide/components.html',
+        controller: 'GuideController',
+        controllerAs: 'ctrl'
+      })
+      .state('layout', {
+        url: '/layout',
+        templateUrl: 'guide/layout.html'
+      });
+  };
 
 })();
