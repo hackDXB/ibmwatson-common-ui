2909ac3b9f8e3a4b22713df6f8146e5e0ee9fff2
David George <david.george@uk.ibm.com>
2016-12-05 13:14:41 +0000

    feat(navigation): Add loading indicator to navigation (#148)
    Add a loading indictator to navigation menu that will display when either
    - config is a promise
    - config.sections is a promise
    
    Break words for long section items
    
    Part of watson-engagement-advisor/wea-backlog#2889
    
    DCO 1.1 Signed-off-by: David George <david.george@uk.ibm.com>

--- src/ng/directives/navigation/navigation.controller.spec.js
+++ src/ng/directives/navigation/navigation.controller.spec.js
@@ -18,142 +18,271 @@
 
 describe('Controller: WorkspaceController', function testWorkspaceController () {
 
-  var vm, $scope, TEST_DATA, MOCKS;
+  var vm, $scope, $q, TEST_DATA, MOCKS;
 
-  MOCKS = {
-    LOCATION : {
-      url : function () {
-        return '/elements';
+  beforeEach(function () {
+
+    MOCKS = {
+      LOCATION : {
+        url : function () {
+          return '/elements';
+        }
       }
-    }
-  };
+    };
 
-  TEST_DATA = {
-    CONFIG : {
-      'label' : 'Menu',
-      'sections' : [
-        {
-          'label' : 'Back somewhere',
-          'icon' : 'ibm-glyph--next-right-forward_24 rotate-180',
-          'href' : '#/overview'
-        },
-        {
-          'label' : 'Main',
-          'icon' : 'ibm-icon--home',
+    TEST_DATA = {
+      CONFIG : {
+        'label' : 'Menu',
+        'sections' : [
+          {
+            'label' : 'Back somewhere',
+            'icon' : 'ibm-glyph--next-right-forward_24 rotate-180',
+            'href' : '#/overview'
+          },
+          {
+            'label' : 'Main',
+            'icon' : 'ibm-icon--home',
+            'links' : [
+              {
+                'label' : 'Overview',
+                'href' : '#/overview',
+                'icon' : 'ibm-icon--home'
+              },
+              {
+                'label' : 'Elements',
+                'href' : '#/elements',
+                'icon' : 'ibm-icon--code'
+              },
+              {
+                'label' : 'Components',
+                'href' : '#/components',
+                'icon' : 'ibm-icon--spaces'
+              }
+            ]
+          },
+          {
+            'label' : 'Other',
+            'icon' : 'ibm-icon--spaces',
+            'links' : [
+              {
+                'label' : 'Layout',
+                'href' : '#/layout',
+                'icon' : 'ibm-icon--spaces'
+              }
+            ]
+          }
+        ],
+        'footer' : {
           'links' : [
             {
-              'label' : 'Overview',
+              'label' : 'Settings',
               'href' : '#/overview',
-              'icon' : 'ibm-icon--home'
+              'icon' : 'ibm-icon--settings-manage'
             },
             {
-              'label' : 'Elements',
-              'href' : '#/elements',
-              'icon' : 'ibm-icon--code'
-            },
-            {
-              'label' : 'Components',
-              'href' : '#/components',
-              'icon' : 'ibm-icon--spaces'
-            }
-          ]
-        },
-        {
-          'label' : 'Other',
-          'icon' : 'ibm-icon--spaces',
-          'links' : [
-            {
-              'label' : 'Layout',
-              'href' : '#/layout',
-              'icon' : 'ibm-icon--spaces'
+              'label' : 'Log out',
+              'href' : '#/overview',
+              'icon' : 'ibm-icon--insert'
             }
           ]
         }
-      ],
-      'footer' : {
-        'links' : [
-          {
-            'label' : 'Settings',
-            'href' : '#/overview',
-            'icon' : 'ibm-icon--settings-manage'
-          },
-          {
-            'label' : 'Log out',
-            'href' : '#/overview',
-            'icon' : 'ibm-icon--insert'
-          }
-        ]
       }
-    }
-  };
+    };
 
-  // load the controller's module
-  beforeEach(function () {
-    module('watson.common.ui');
+  });
 
-    inject(function ($injector, $controller, _$rootScope_) {
-      $scope = _$rootScope_.$new();
-      $scope.config = TEST_DATA.CONFIG;
+  describe('Config as object', function () {
 
-      vm = $controller('NavigationController', {
-        $scope : $scope,
-        $location : MOCKS.LOCATION
+    // load the controller's module
+    beforeEach(function () {
+      module('watson.common.ui');
+
+      inject(function ($injector, $controller, _$rootScope_) {
+        $scope = _$rootScope_.$new();
+        $scope.config = TEST_DATA.CONFIG;
+
+        vm = $controller('NavigationController', {
+          $scope : $scope,
+          $location : MOCKS.LOCATION
+        });
       });
     });
+
+    describe('.toggleMenu', function () {
+      it('should toggle the menu', function () {
+        vm.toggleMenu();
+        vm.menuOpen.should.equal(true);
+        vm.toggleMenu();
+        vm.menuOpen.should.equal(false);
+      });
+
+      it('should close the menu', function () {
+        vm.toggleMenu(false);
+        vm.menuOpen.should.equal(false);
+      });
+
+      it('should open the menu', function () {
+        vm.toggleMenu(true);
+        vm.toggleMenu(true);
+        vm.menuOpen.should.equal(true);
+      });
+    });
+
+    describe('.setCurrent', function () {
+      it('should set the current link', function () {
+        var section = {'label' : 'section'};
+        var link = {'label' : 'link'};
+        vm.setCurrent(section, link);
+        vm.currentSection.should.equal(section);
+        vm.currentLink.should.equal(link);
+      });
+
+      it('should not set null section', function () {
+        var section = {'label' : 'section'};
+        var link = {'label' : 'link'};
+        vm.setCurrent(section, link);
+        vm.currentSection.should.equal(section);
+        vm.currentLink.should.equal(link);
+        vm.setCurrent(null, null);
+        vm.currentSection.should.equal(section);
+        should.not.exist(vm.currentLink);
+      });
+    });
+
+    describe('.toggleDetails', function () {
+      it('should set detailsOpen to true', function () {
+        var link = {'label' : 'link', 'details' : ['one', 'two', 'three']};
+        vm.toggleDetails(link);
+        link.detailsOpen.should.be.true;
+      });
+
+      it('should set detailsOpen to false', function () {
+        var link = {'label' : 'link', 'details' : ['one', 'two', 'three'], 'detailsOpen' : true};
+        vm.toggleDetails(link);
+        link.detailsOpen.should.be.false;
+      });
+    });
+
   });
 
-  describe('.toggleMenu', function () {
-    it('should toggle the menu', function () {
-      vm.toggleMenu();
-      vm.menuOpen.should.equal(true);
-      vm.toggleMenu();
-      vm.menuOpen.should.equal(false);
+  describe('sections as promise', function () {
+    // load the controller's module
+    beforeEach(function () {
+      module('watson.common.ui');
+
+      inject(function ($injector, $controller, _$rootScope_, _$q_) {
+        $q = _$q_;
+
+        $scope = _$rootScope_.$new();
+
+        this.configPromise = $q.defer();
+
+        $scope.config = this.configPromise.promise;
+
+        vm = $controller('NavigationController', {
+          $scope : $scope,
+          $location : MOCKS.LOCATION
+        });
+      });
     });
 
-    it('should close the menu', function () {
-      vm.toggleMenu(false);
-      vm.menuOpen.should.equal(false);
+    it('should setup the navigation links once the config is resolved', function () {
+      vm.should.not.have.a.property('label');
+      vm.sections.length.should.equal(0);
+      vm.loading.should.be.true;
+
+      this.configPromise.resolve(TEST_DATA.CONFIG);
+      $scope.$apply();
+
+      vm.loading.should.be.false;
+      vm.label.should.equal(TEST_DATA.CONFIG.label);
+      vm.sections.length.should.equal(TEST_DATA.CONFIG.sections.length);
+
     });
 
-    it('should open the menu', function () {
-      vm.toggleMenu(true);
-      vm.toggleMenu(true);
-      vm.menuOpen.should.equal(true);
+    it('should remove the loading indicator if the promise is rejected', function () {
+      vm.should.not.have.a.property('label');
+      vm.sections.length.should.equal(0);
+      vm.loading.should.be.true;
+
+      this.configPromise.reject(new Error());
+      $scope.$apply();
+
+      vm.should.not.have.a.property('label');
+      vm.sections.length.should.equal(0);
+      vm.loading.should.be.false;
+
     });
+
   });
 
-  describe('.setCurrent', function () {
-    it('should set the current link', function () {
-      var section = {'label' : 'section'};
-      var link = {'label' : 'link'};
-      vm.setCurrent(section, link);
-      vm.currentSection.should.equal(section);
-      vm.currentLink.should.equal(link);
+  describe('sections as promise', function () {
+    // load the controller's module
+    beforeEach(function () {
+      module('watson.common.ui');
+
+      inject(function ($injector, $controller, _$rootScope_, _$q_) {
+        $q = _$q_;
+
+        $scope = _$rootScope_.$new();
+
+        this.sections = TEST_DATA.CONFIG.sections;
+        this.sectionPromise = $q.defer();
+
+        TEST_DATA.CONFIG.sections = this.sectionPromise.promise;
+
+        $scope.config = TEST_DATA.CONFIG;
+
+        vm = $controller('NavigationController', {
+          $scope : $scope,
+          $location : MOCKS.LOCATION
+        });
+      });
     });
 
-    it('should not set null section', function () {
-      var section = {'label' : 'section'};
-      var link = {'label' : 'link'};
-      vm.setCurrent(section, link);
-      vm.currentSection.should.equal(section);
-      vm.currentLink.should.equal(link);
-      vm.setCurrent(null, null);
-      vm.currentSection.should.equal(section);
-      should.not.exist(vm.currentLink);
+    it('should setup the navigation links once the config is resolved', function () {
+
+      vm.should.not.have.a.property('label');
+      vm.sections.length.should.equal(0);
+      vm.loading.should.be.true;
+
+      $scope.$apply();
+
+      vm.label.should.equal(TEST_DATA.CONFIG.label);
+      vm.sections.length.should.equal(0);
+      vm.loading.should.be.true;
+
+      this.sectionPromise.resolve(this.sections);
+      $scope.$apply();
+
+      vm.loading.should.be.false;
+      vm.sections.length.should.equal(this.sections.length);
+
     });
+
+    it('should remove the loading indicator if the promise is rejected', function () {
+
+      vm.should.not.have.a.property('label');
+      vm.sections.length.should.equal(0);
+      vm.loading.should.be.true;
+
+      $scope.$apply();
+
+      vm.label.should.equal(TEST_DATA.CONFIG.label);
+      vm.sections.length.should.equal(0);
+      vm.loading.should.be.true;
+
+      this.sectionPromise.reject(new Error());
+      $scope.$apply();
+
+      vm.loading.should.be.false;
+      vm.sections.length.should.equal(0);
+
+    });
+
+
   });
 
-  describe('.toggleDetails', function () {
-    it('should set detailsOpen to true', function () {
-      var link = {'label' : 'link', 'details' : ['one', 'two', 'three']};
-      vm.toggleDetails(link);
-      link.detailsOpen.should.be.true;
-    });
 
-    it('should set detailsOpen to false', function () {
-      var link = {'label' : 'link', 'details' : ['one', 'two', 'three'], 'detailsOpen' : true};
-      vm.toggleDetails(link);
-      link.detailsOpen.should.be.false;
-    });
-  });
+
 });
